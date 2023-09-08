a1878aed2c4fe5ec1712c0b6262baabf02706b75
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-09-08 13:07:27 +0300

    add

---
+++ 000-landing-page/app.js
@@ -0,0 +1,5 @@
+const contactButton = document.querySelector('.contact-button');
+        contactButton.addEventListener('click', () => {
+            alert('You clicked the Contact Us button!');
+            // add more code to handle the contact form or other actions.
+});
