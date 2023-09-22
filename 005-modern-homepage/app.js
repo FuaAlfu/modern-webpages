de478e4c1fecf583397093227f672be2754fd224
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2023-09-22 10:54:02 +0300

    add

---
+++ 005-modern-homepage/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const ctaButton = document.getElementById("cta-button");
+    ctaButton.addEventListener("click", function () {
+        alert("You clicked the 'Learn More' button!");
+        // You can add more code here to navigate to another page or perform other actions.
+    });
+});
