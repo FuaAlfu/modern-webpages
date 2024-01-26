80e55681684eb5ba6f7c663ff4e72e77e3d2a953
FuaAlfu <55155669+FuaAlfu@users.noreply.github.com>
2024-01-26 23:37:54 +0300

    add

---
+++ 011-modern-buttons/app.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    document.getElementById('contact-message').style.display = 'block';
+  }
+
+  function hideMessage() {
+    document.getElementById('contact-message').style.display = 'none';
+  }
